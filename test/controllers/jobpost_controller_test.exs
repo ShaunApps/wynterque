9703d40fd001240db41b9a6068a1d57d24b71fd1
ShaunApps <shaun.apps@gmail.com>
2016-09-17 21:18:51 -0400

    additions and refractor of jobpost controller test

--- test/controllers/jobpost_controller_test.exs
+++ test/controllers/jobpost_controller_test.exs
@@ -1,6 +1,27 @@
 defmodule Wynterque.JobpostControllerTest do
   use Wynterque.ConnCase
 
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(conn, :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
+  @tag login_as: "max"
+  test "lists all user's videos on index", %{conn: conn, user: user} do
+    user_jobpost = insert_jobpost(user, title: "Javascript Developer")
+    other_jobpost = insert_jobpost(insert_user(username: "other"), title: "another job")
+
+    conn = get conn, jobpost_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Job Postings/
+    assert String.contains?(conn.resp_body, user_jobpost.title)
+    refute String.contains?(conn.resp_body, other_jobpost.title)
+  end
+
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
       get(conn, jobpost_path(conn, :new)),
