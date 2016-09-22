e5f1e7f36e1c3adba6730994dbda26d377bd83d1
ShaunApps <shaun.apps@gmail.com>
2016-09-21 23:25:27 -0400

    added exid string migration

--- priv/repo/migrations/20160920202535_add_ex_id_to_student.exs
+++ priv/repo/migrations/20160920202535_add_ex_id_to_student.exs
@@ -2,10 +2,10 @@ defmodule Wynterque.Repo.Migrations.AddExIdToStudent do
   use Ecto.Migration
 
   def change do
-    alter_table(:students) do
+    alter table(:students) do
       add :ex_id, :string
     end
 
-    creat unique_index(:students, [:ex_id])
+    create unique_index(:students, [:ex_id])
   end
 end
